a0e5fc2da85a67f0b87fbfb3906be289e5f5e5a4
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2023-08-16 08:02:56 -0300

    feat: users new props, clock table and microsoft provider

---
+++ src/api/employee-clock/controllers/employee-clock.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * employee-clock controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::employee-clock.employee-clock");
---
+++ src/api/employee-clock/routes/employee-clock.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * employee-clock router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::employee-clock.employee-clock');
